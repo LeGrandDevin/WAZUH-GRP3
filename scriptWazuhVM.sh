582e29c41eb8f873e459ea019f06ce177057fc95
Yatavv <129736678+Yatavv@users.noreply.github.com>
2023-07-01 17:47:19 +0200

    Scripts
    Installation et configuration d'ansible
    Configuration du remote system (wazuh)
    Mise en place d'un auto update des règles OSSEC

---
+++ scriptWazuhVM.sh
@@ -0,0 +1,22 @@
+#2nd VM Debian for Wazuh Indexer, Dashboard and Manager
+#This script must be executed before scriptAnsibleVM.sh is executed on the Ansible server
+
+#Preparation for installing wazuh on the remote system
+sudo su
+expect 'password: '
+send 'root/r'
+cd ~
+
+#Install openssh-server
+apt-get install openssh-server
+
+#Start SSH service
+systemctl start sshd
+
+#Create .ssh directory and set permissions
+mkdir .ssh
+chmod 700 .ssh/
+
+#Create authorized_keys file and set permissions
+touch .ssh/authorized_keys
+chmod 644 .ssh/authorized_keys
